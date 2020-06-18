e1420b582ce54fb1ae2f6e7955713ef9d6df2039
abhishek.bodhekar <abhishek.bodhekar@t-systems.com>
2020-06-18 20:12:42 +0530

    added few things

--- main.go
+++ main.go
@@ -6,19 +6,54 @@ import (
 	"os"
 )
 
-func main() {
+const basicCode = "package main \n func main() {\n"
 
+func main() {
 	if len(os.Args) != 2 {
 		Usage()
 		return
 	}
+	file := CreateFile()
+	defer ClearAll(file.Name())
+	WriteAndRenameFile(file)
 
 }
 
-func CreateFile() {
-	ioutil.TempFile(".", "temp")
+func WriteAndRenameFile(file *os.File) *os.File {
+	code := basicCode + os.Args[1] + "\n}"
+	file.Write([]byte(code))
+	err := os.Rename(file.Name(), file.Name()+".go")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Can rename temp file : "+err.Error()+"\n")
+		os.Exit(2)
+
+	}
+	return file
+
+}
+
+func CreateFile() *os.File {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Can not get currrent directory : "+err.Error()+"\n")
+		os.Exit(2)
+	}
+
+	file, err := ioutil.TempFile(currentDir, "temp")
+	if err != nil {
+		fmt.Fprint(os.Stderr, "Can not create temp file : "+err.Error()+"\n")
+		os.Exit(2)
+	}
+	return file
+
 }
 func Usage() {
-	fmt.Fprint(os.Stderr, "Usage : gorun [_GoCode_]\n")
+	fmt.Fprint(os.Stderr, "Usage : gorun [_Body_Of_main()_]\n")
+	os.Exit(2)
+}
+
+func ClearAll(name string) {
+	fmt.Println("Clearing, " + name)
+	os.Remove(name)
 	os.Exit(2)
 }
